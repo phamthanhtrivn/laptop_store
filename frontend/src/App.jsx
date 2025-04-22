3f6b31541b66eb4bd0fe5f739ea38c2f9bc71524
Tri Pham <phamthanhtri0712@gmail.com>
2025-04-22 09:03:01 +0700

    static layout user detail

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
-import User from "./pages/User";
 import Header from "./components/Header";
 import { ToastContainer } from 'react-toastify';
 import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
 import Login from "./pages/Login";
+import UserDetail from "./pages/UserDetail";
 
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/user" element={<User />} />
+          <Route path="/user" element={<UserDetail />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/products" element={<Products />} />
           <Route path="/login" element={<Login />} />
